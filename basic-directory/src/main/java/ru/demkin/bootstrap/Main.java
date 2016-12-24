bf0e37c769729d9b7d56c3c301383a86baf27dec
Eugene <Evgeny.Demkin@moex.com>
2016-12-24 13:31:27 +0300

    initial project

---
+++ basic-directory/src/main/java/ru/demkin/bootstrap/Main.java
@@ -0,0 +1,11 @@
+package ru.demkin.bootstrap;
+
+/**
+ * @author evgeny.demkin@moex.com
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
