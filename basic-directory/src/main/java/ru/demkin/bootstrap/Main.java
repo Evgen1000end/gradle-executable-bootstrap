59335c25f657cd4805462f6d586767127217b783
Eugene <Evgeny.Demkin@moex.com>
2016-12-24 14:27:11 +0300

    copy config in folder

--- basic-directory/src/main/java/ru/demkin/bootstrap/Main.java
+++ basic-directory/src/main/java/ru/demkin/bootstrap/Main.java
@@ -1,9 +1,13 @@
 package ru.demkin.bootstrap;
 
+import java.util.List;
+
 /**
  * @author evgeny.demkin@moex.com
  */
 public class Main {
+
+  public List<String> s;
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
